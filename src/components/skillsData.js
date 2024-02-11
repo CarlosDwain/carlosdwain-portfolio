a4b9f49b47212c15de3e53178e57b91cded6688a
Carlos Dwain Sorallo <sorallo.carlos17@gmail.com>
2024-02-11 13:26:44 +0800

    added matlab in my skills

--- src/components/skillsData.js
+++ src/components/skillsData.js
@@ -3,7 +3,8 @@ const skillsData = [
     // { name: 'CSS', iconClass: 'fa-brands fa-css3-alt css-icon' },
     // { name: 'JavaScript', iconClass: 'fa-brands fa-js-square' },
     { name: 'React', iconClass: 'fa-brands fa-react' },
-    { name: 'C++', iconClass: 'icon-cplusplus' },
+    { name: 'C++', iconClass: '' },
+    { name: 'MATLAB', iconClass: '' },
     // { name: 'Node', iconClass: 'fa-brands fa-node-js node-icon' },
     { name: 'Python', iconClass: 'fa-brands fa-python' },
   ];
