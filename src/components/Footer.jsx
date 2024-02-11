06d129030ae7e98150db933f2599c493071e5e3e
Carlos Dwain Sorallo <sorallo.carlos17@gmail.com>
2024-02-11 13:22:32 +0800

    added reference at the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,41 +1,40 @@
 const Footer = () => {
-    return (
-      <>
-        <hr/>
-        <div className="footer-container">
-          <p>
-            © {new Date().getFullYear()} Carlos' Portfolio
-          </p>
-          <div className="social_icons">
-            <a
-              href="https://twitter.com/CarlosDwain"
-              aria-label="Twitter"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-twitter"></i>
-            </a>
-            <a
-              href="https://github.com/CarlosDwain"
-              aria-label="GitHub"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-github"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/carlos-dwain-sorallo-54a596154/"
-              aria-label="LinkedIn"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-linkedin"></i>
-            </a>
-          </div>
+  return (
+    <>
+      <hr/>
+      <div className="footer-container">
+        <p>
+          © {new Date().getFullYear()} Carlos' Portfolio | Template designed by Joel Olawanle
+        </p>
+        <div className="social_icons">
+          <a
+            href="https://twitter.com/CarlosDwain"
+            aria-label="Twitter"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa-brands fa-twitter"></i>
+          </a>
+          <a
+            href="https://github.com/CarlosDwain"
+            aria-label="GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/carlos-dwain-sorallo-54a596154/"
+            aria-label="LinkedIn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
         </div>
-      </>
-    )
-  }
-  
-  export default Footer;
+      </div>
+    </>
+  )
+}
 
+export default Footer;
