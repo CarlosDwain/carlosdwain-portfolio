ed3f33805880643366b72c09fa704d1e18e4571e
Carlos Dwain Sorallo <sorallo.carlos17@gmail.com>
2024-10-08 18:40:25 +0800

    Modified profile

--- src/components/skillsData.js
+++ src/components/skillsData.js
@@ -5,7 +5,7 @@ const skillsData = [
     { name: 'Python', iconClass: 'fa-brands fa-python' },
     { name: 'SQL', iconClass: 'fa-solid fa-database' },
     { name: 'git', iconClass: 'fa-brands fa-git' },
-    { name: 'MATLAB', iconClass: 'icon-cplusplus' },
+    { name: 'MATLAB', iconClass: '' },
     { name: 'C++', iconClass: '' },
     // { name: 'Node', iconClass: 'fa-brands fa-node-js node-icon' },
     
