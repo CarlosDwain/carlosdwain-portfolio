8a8a6cc3169e32f2862e7fe6a15c4a09088d79b2
Carlos Dwain Sorallo <sorallo.carlos17@gmail.com>
2025-05-03 12:11:23 +0800

    Update about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,16 +6,14 @@ const About = () => {
       <h2>About Me</h2>
       <div className="flex-about">
         <div className="about-text">
-          <p>My passion for data sparked when I took my Data Science and Artificial Intelligence classes when I was 
-            in my 4th year in college. Before that, I have no idea on how Python works, I only know c++ back then because
-            I was in a Science High School where I have robotics class. After that, I took my thesis and it was all 
-            about training a deep learning model. I took Coursera courses and joined the Huawei Certified ICT 
-            Associate - Artificial Intelligence (HCIA-AI) just to widen my understanding on data.  
+          <p>My passion for data began in my fourth year of college when I took Data Science and Artificial Intelligence courses. 
+          At the time, I had no experience with Python—my only programming background was in C++, which I learned in Science High School through a robotics class. 
+          That turning point opened a new world for me.</p>
+          <p>During my thesis, I dove deeper by training a deep learning model, which fueled my curiosity even more. 
+          To expand my knowledge, I took online courses on Coursera and earned the Huawei Certified ICT Associate – Artificial Intelligence (HCIA-AI) certification.</p>
+          <p>Since then, I’ve continued to grow in the field. I’m currently taking the Data Engineering track on DataCamp to sharpen my skills and deepen my understanding. 
+          Data continues to amaze me, and I’m excited to see where this passion will take me next.
           </p>
-          <p>Fast forward today, I learned a lot and I plan to continue growing. I am taking the Datacamp courses on 
-          the data engineering track. It has been a year or two now when I first embark my journey on data. Until now, 
-          data still amazes me and I think I just found my passion. Well, let's see on how far I can take this passion. 
-          Thank you for reading!</p> 
         </div>
         <div className="about-img">
           <Image src='/images/profile2.jpg' className="profile-img" width={500} height={500}/>
