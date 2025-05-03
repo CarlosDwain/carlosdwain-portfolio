e6476394782fe7c1332301a0fd9157e22b5c5c61
Carlos Dwain Sorallo <sorallo.carlos17@gmail.com>
2025-05-03 18:28:53 +0800

    Update projects page

--- src/components/data.js
+++ src/components/data.js
@@ -17,7 +17,7 @@ export const projectData = [
       id: 3,
       title: 'Real-Time-Weather-Data-Pipeline-for-Philippine-Cities',
       description:
-        '[on-going] A complete data engineering pipeline project that fetches weather data in real-time for cities in the Philippines',
+        'A complete data engineering pipeline project that fetches weather data in real-time for cities in the Philippines and stores it in SQL server.',
       gitHubLink: 'https://github.com/CarlosDwain/Real-Time-Weather-Data-Pipeline-for-Philippine-Cities',
     },
   ];
